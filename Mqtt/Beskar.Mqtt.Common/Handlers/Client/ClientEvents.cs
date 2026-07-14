650babb7af6cc30e180b5bba32c7f00155180ca6
Marvin Drude <marvindrude1@gmail.com>
2026-07-14 20:28:09 +0200

    Add handler pipelines for new events in client

--- Mqtt/Beskar.Mqtt.Common/Handlers/Client/ClientEvents.cs
+++ Mqtt/Beskar.Mqtt.Common/Handlers/Client/ClientEvents.cs
@@ -6,4 +6,10 @@ namespace Beskar.Mqtt.Common.Handlers.Client;
 public sealed class ClientEvents
 {
    public readonly HandlerPipeline<MessageReceiveContext> OnMessageReceive = new();
+
+   public readonly HandlerPipeline<ClientConnectingContext> OnClientConnecting = new();
+
+   public readonly HandlerPipeline<ClientConnectedContext> OnClientConnected = new();
+
+   public readonly HandlerPipeline<ClientDisconnectedContext> OnClientDisconnected = new();
 }
