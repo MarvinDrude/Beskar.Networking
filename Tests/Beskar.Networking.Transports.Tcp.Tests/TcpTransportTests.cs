3235b9d0ce7bb7e8455dc3d042c025d390e03675
Marvin Drude <marvindrude1@gmail.com>
2026-07-20 17:02:49 +0200

    new test for underyling transports

--- Tests/Beskar.Networking.Transports.Tcp.Tests/TcpTransportTests.cs
+++ Tests/Beskar.Networking.Transports.Tcp.Tests/TcpTransportTests.cs
@@ -2,6 +2,7 @@ using System.Buffers;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
+using Beskar.Networking.Abstractions.Interfaces;
 using Beskar.Networking.Transports.Quic;
 
 namespace Beskar.Networking.Transports.Tcp.Tests;
@@ -573,6 +574,101 @@ public class TcpTransportTests
       // Verify that the semaphore count is STILL 1 (not leaked)
       await Assert.That(semaphore.CurrentCount).IsEqualTo(1);
 
+      await listener.UnbindAsync();
+   }
+
+   [Test]
+   public async Task TcpClientServer_MultipleClients_DataExchangedWithoutLeakage()
+   {
+      var options = new TcpTransportOptions();
+      var listener = new TcpNetworkListener(new IPEndPoint(IPAddress.Loopback, 0), options);
+
+      var bindResult = await listener.BindAsync();
+      await Assert.That(bindResult.Failed).IsFalse();
+
+      var client1 = new TcpNetworkClient(options);
+      var client2 = new TcpNetworkClient(options);
+
+      var connectResult1 = await client1.ConnectAsync(listener.LocalAddress);
+      var connectResult2 = await client2.ConnectAsync(listener.LocalAddress);
+
+      await Assert.That(connectResult1.Failed).IsFalse();
+      await Assert.That(connectResult2.Failed).IsFalse();
+
+      var clientSession1 = connectResult1.Success!;
+      var clientSession2 = connectResult2.Success!;
+
+      var acceptResult1 = await listener.AcceptSessionAsync();
+      var acceptResult2 = await listener.AcceptSessionAsync();
+
+      await Assert.That(acceptResult1.Failed).IsFalse();
+      await Assert.That(acceptResult2.Failed).IsFalse();
+
+      var serverSession1 = acceptResult1.Success!;
+      var serverSession2 = acceptResult2.Success!;
+
+      var clientStream1 = (await clientSession1.OpenStreamAsync()).Success!;
+      var clientStream2 = (await clientSession2.OpenStreamAsync()).Success!;
+
+      var identity1 = "Client1-Identity"u8.ToArray();
+      await clientStream1.Transport.Output.WriteAsync(identity1);
+      await clientStream1.Transport.Output.FlushAsync();
+
+      var identity2 = "Client2-Identity"u8.ToArray();
+      await clientStream2.Transport.Output.WriteAsync(identity2);
+      await clientStream2.Transport.Output.FlushAsync();
+
+      var serverStream1 = (await serverSession1.AcceptStreamAsync()).Success!;
+      var serverStream2 = (await serverSession2.AcceptStreamAsync()).Success!;
+
+      var serverReadResult1 = await serverStream1.Transport.Input.ReadAsync();
+      var serverReadBytes1 = serverReadResult1.Buffer.ToArray();
+      serverStream1.Transport.Input.AdvanceTo(serverReadResult1.Buffer.End);
+
+      var serverReadResult2 = await serverStream2.Transport.Input.ReadAsync();
+      var serverReadBytes2 = serverReadResult2.Buffer.ToArray();
+      serverStream2.Transport.Input.AdvanceTo(serverReadResult2.Buffer.End);
+
+      INetworkStream serverStreamForClient1;
+      INetworkStream serverStreamForClient2;
+
+      if (serverReadBytes1.SequenceEqual(identity1))
+      {
+         serverStreamForClient1 = serverStream1;
+         serverStreamForClient2 = serverStream2;
+         await Assert.That(serverReadBytes2).IsEquivalentTo(identity2);
+      }
+      else
+      {
+         serverStreamForClient1 = serverStream2;
+         serverStreamForClient2 = serverStream1;
+         await Assert.That(serverReadBytes1).IsEquivalentTo(identity2);
+         await Assert.That(serverReadBytes2).IsEquivalentTo(identity1);
+      }
+
+      var msg1 = "Payload For Client 1"u8.ToArray();
+      var msg2 = "Payload For Client 2"u8.ToArray();
+
+      await serverStreamForClient1.Transport.Output.WriteAsync(msg1);
+      await serverStreamForClient1.Transport.Output.FlushAsync();
+
+      await serverStreamForClient2.Transport.Output.WriteAsync(msg2);
+      await serverStreamForClient2.Transport.Output.FlushAsync();
+
+      var clientReadResult1 = await clientStream1.Transport.Input.ReadAsync();
+      var clientReadBytes1 = clientReadResult1.Buffer.ToArray();
+      clientStream1.Transport.Input.AdvanceTo(clientReadResult1.Buffer.End);
+      await Assert.That(clientReadBytes1).IsEquivalentTo(msg1);
+
+      var clientReadResult2 = await clientStream2.Transport.Input.ReadAsync();
+      var clientReadBytes2 = clientReadResult2.Buffer.ToArray();
+      clientStream2.Transport.Input.AdvanceTo(clientReadResult2.Buffer.End);
+      await Assert.That(clientReadBytes2).IsEquivalentTo(msg2);
+
+      await clientSession1.DisposeAsync();
+      await clientSession2.DisposeAsync();
+      await serverSession1.DisposeAsync();
+      await serverSession2.DisposeAsync();
       await listener.UnbindAsync();
    }
 }
